79f17116e54a9afc34616a7b3ca2f720ac588e2c
FERRYSELING <smartmzl@qq.com>
2018-08-04 09:32:49 +0800

    new functions

--- CryptographerLab/large_number_calculation.cpp
+++ CryptographerLab/large_number_calculation.cpp
@@ -1,8 +1,9 @@
 #include "stdafx.h"
 #include "stdio.h"
-#include "string.h"
 #include <stdlib.h>
+#include <iostream>
 #include "large_number_calculation.h"
+
 
 large_number_calculation::large_number_calculation()
 {
@@ -18,47 +19,140 @@ large_number_calculation* large_number_calculation::create() {
 	return obj;
 }
 
-void large_number_calculation::inputHex(int num_index, const char* input) {
+void large_number_calculation::runMultiple() {
+	// 检查A和B是否有设置过值
+	if (this->isNumberSet != 3) return;
+	// 
+}
+
+void large_number_calculation::multipleSingleNumber(unsigned int A, unsigned int singleNumber, unsigned int &result, unsigned short &carryNumber) {
+	// 
+	unsigned long int mr = A * singleNumber;
+	// 进位得到的数
+	unsigned int carryNumber = 0;
+	if (mr < A && mr < singleNumber ) {
+
+	}
+}
+
+void large_number_calculation::runAdd() {
+	// 检查A和B是否有设置过值
+	if (this->isNumberSet != 3) return;
 	// 初始化结构体
 	largeInt *head = (largeInt*)malloc(sizeof(largeInt));
 	largeInt *tail = head;
 	largeInt *current = head;
-	(*current).v = 0; (*current).next = NULL;
+	(*current).v = 0; (*current).next = NULL; (*current).prev = NULL;
+	// largeInt指针
+	largeInt *pA = this->numA_tail, *pB = this->numB_tail;
+	// 进位标记
+	bool ifCarry = false;
+	// 计数
+	unsigned int count = 0;
+	// 开始执行加法
+	while (pA != NULL || pB != NULL || ifCarry)
+	{
+		// 取当前int的值，并求加法
+		unsigned int A = pA == NULL ? 0 : (*pA).v;
+		unsigned int B = pB == NULL ? 0 : (*pB).v;
+		unsigned int R = 0;
+
+		// 检查是否会有溢出
+		// 注意一种情况：A=B=0xFFFFFFFF，并且ifCarry=true
+		if (A == UINT_MAX && B == UINT_MAX && ifCarry == true) {
+			R = UINT_MAX;
+			// 下次进位
+			ifCarry = true;
+		}else if (A > UINT_MAX - B - (ifCarry ? 1 : 0)) {
+			// 溢出进位
+			unsigned int tmp_add = UINT_MAX - A + 1;
+			R = B + (ifCarry ? 1 : 0) - tmp_add;
+			// 下次进位
+			ifCarry = true;
+		}
+		else {
+			// 未溢出不进位
+			R = A + B + (ifCarry ? 1 : 0);
+			// 下次不进位
+			ifCarry = false;
+		}
+		// 设置值
+		(*current).v = R;
+		// 移至下一个
+		if (pA != NULL || pB != NULL) {
+			pA = (*pA).prev;
+			pB = (*pB).prev;
+		}
+		// 创建新节点
+		if (pA != NULL || pB != NULL || ifCarry) {
+			largeInt *tmp = (largeInt*)malloc(sizeof(largeInt));
+			(*tmp).v = 0; (*tmp).next = current; (*tmp).prev = NULL; (*current).prev = tmp;
+			current = tmp; head = current;
+		}
+	}
+
+	// 求出A和B中位数最大的位数的值
+	unsigned int max = this->bitsA > this->bitsB ? this->bitsA : this->bitsB;
+
+	this->bitsC = ifCarry ? max + 1 : max;
+	this->numC = head;
+	this->numC_tail = tail;
+}
+
+void large_number_calculation::inputHex(int num_index, const char* input) {
+	// 检查当前要设置的位是否被设置过
+	if (num_index == 0 && this->isNumberSet & 1 == 0) {
+		return;
+	}
+	if (num_index == 1 && this->isNumberSet & 2 == 0) {
+		return;
+	}
+	// 初始化结构体
+	largeInt *head = (largeInt*)malloc(sizeof(largeInt));
+	largeInt *tail = head;
+	largeInt *current = head;
+	(*current).v = 0; (*current).next = NULL; (*current).prev = NULL;
 	// 当前读取的元素和计数
 	int p = *input;
 	int count = 0;
 	// 每个int存储8个十六进制位，这个变量存储当前已经存储了多少
-	int current_int_load = 0;
+	short int current_int_load = 0;
 	// 应当从低位开始存储，因此先把指针放在最末尾
 	while (p != '\0') {
 		p = *(input + ++count);
 	}
 	// 开始存储
-	int index = count - 1;
+	int index = count-1;
 	while (index >= 0)
 	{
 		// 如果当前largeInt节点已经存满，则再创建一个
 		if (current_int_load >= 8) {
 			largeInt *tmp = (largeInt*)malloc(sizeof(largeInt));
-			(*tmp).v = 0; (*tmp).next = current;
-			current = tmp; head = current;
+			(*tmp).v = 0; (*tmp).next = current; (*tmp).prev = NULL; (*current).prev = tmp;
+			current = tmp; head = current; 
 			current_int_load = 0;
-			printf_s("----- new int -----");
 		}
 		// 读取字符串值并格式化
 		p = this->hexToDecBit(*(input + index--));
-
-		printf_s("%s", *(input + index--));
-		
-		// 输入到结构体中
-		int tmp = p << (current_int_load * 4);
-		(*current).v += tmp;
-		current_int_load++;
+		if (p >= 0) {
+			// 如果输入合法输入到结构体中
+			int tmp = p << (current_int_load * 4);
+			(*current).v += tmp;
+			current_int_load++;
+		}
 	}
-
-	this->numA = head;
-	this->numA_tail = tail;
-
+	if (num_index == 0) {
+		this->bitsA = count * 4;
+		this->numA = head;
+		this->numA_tail = tail;
+		this->isNumberSet = this->isNumberSet | 1;
+	}
+	else if (num_index == 1) {
+		this->bitsB = count * 4;
+		this->numB = head;
+		this->numB_tail = tail;
+		this->isNumberSet = this->isNumberSet | 2;
+	}
 }
 
 short int large_number_calculation::hexToDecBit(const char num) {
@@ -72,4 +166,35 @@ short int large_number_calculation::hexToDecBit(const char num) {
 	else if (num >= 48 and num <= 57) {
 		return num - 48;
 	}
+	else {
+		return -1;
+	}
+}
+
+void large_number_calculation::getValue(int num_index) {
+	unsigned int bits_count = 0;
+	largeInt *head;
+	if (num_index == 0) {
+		head = this->numA;
+	}
+	else if (num_index == 0) {
+		head = this->numB;
+	}
+	else {
+		head = this->numC;
+	}
+
+	largeInt *current = head;
+	while (current != NULL)
+	{
+		unsigned int num = (*current).v;
+		for (size_t i = 8; i > 0; i--)
+		{
+			int offset = i - 1;
+			unsigned int tmp = (num & (0xF << (offset * 4))) >> (offset * 4);
+			std::cout << this->hex2char[tmp];
+		}
+		current = (*current).next;
+	}
+	std::cout << std::endl;
 }
--- CryptographerLab/large_number_calculation.h
+++ CryptographerLab/large_number_calculation.h
@@ -6,6 +6,7 @@
 typedef struct largeInt {
 	unsigned int v;
 	largeInt *next;
+	largeInt *prev;
 }largeInt;
 
 class large_number_calculation
@@ -22,7 +23,7 @@ public:
 	void inputHex(int num_index, const char* input);
 	
 	// 获取运算结果
-	char* getResult();
+	void getValue(int num_index);
 	
 	// 运算
 	void runAdd();
@@ -32,13 +33,26 @@ public:
 
 private:
 
+	// 是否已经设置了值
+	// 0:未设置 1:A设置了 2:B设置了 3:A和B都设置了
+	short isNumberSet = 0;
+
+	// 大数寄存器
 	largeInt * numA, *numA_tail;
 	largeInt * numB, *numB_tail;
 	largeInt * numC, *numC_tail;
-	largeInt * numD, *numD_tail;
+
+	// 位数计数
+	int bitsA = 0, bitsB = 0, bitsC = 0;
 
 	// 将一个十六进制位转换成一个十进制数
 	short int hexToDecBit(const char a);
+
+	// 乘数乘以乘以一个十进制数
+	void multipleSingleNumber(unsigned int A, unsigned int singleNumber, unsigned int &result, unsigned short &carryNumber);
+
+	// hex2char
+	char hex2char[16] = { '0' ,'1' ,'2' ,'3' ,'4' ,'5' ,'6' ,'7' ,'8' ,'9' ,'A' ,'B' ,'C' ,'D' ,'E','F' };
 
 };
 
